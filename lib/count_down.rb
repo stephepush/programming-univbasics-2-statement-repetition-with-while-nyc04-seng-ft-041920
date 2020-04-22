5610c76f72915a97f6d243f21b56a394cb996b8c
Stephen Peters <7speter@gmail.com>
2020-04-22 15:49:08 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,7 @@
 # Write your code here
-first_second_of_year = 0
-while count < 10 && count != first_second_of_year do
-  puts "Happy New Year!"
-  count = count -1
+count = 10
+while count >= 1 do
+  puts count
+  count -= 1
 end
+puts "Happy New Year!"
